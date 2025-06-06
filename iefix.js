1db8a82cc24f5be95a1cff47ffe0035e8f616217
GreenSoupDev <109924369+GreenSoupDeveloper@users.noreply.github.com>
2025-06-06 21:52:57 +0200

    Update iefix.js

--- iefix.js
+++ iefix.js
@@ -16,7 +16,7 @@ function theDudeAbidesSplash(){
 }
 
 function theDudeAbides(){
-	/*
+	
 	document.write('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="750" height="375">');
 	document.write('<param name=movie value="flash/maxpayne_main.swf">');
 	document.write('<param name=quality value=high>');
@@ -28,7 +28,7 @@ function theDudeAbides(){
 	document.write('<NOEMBED><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 4 or higher to view this</a></NOEMBED>');
 	document.write('<NOSCRIPT><a href="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash">you must download flash 4 or higher to view this</a></NOSCRIPT>');
 	document.write('</object>');
-	*/
-	document.writeln("<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\"> <param name=\"quality\" value=\"high\"> <param name=\"wmode\" value=\"transparent\"> <param name=\"bgcolor\" value=\"#000000\"> <embed src=\"flash/maxpayne_main.swf\" quality=\"high\" width=\"750\" height=\"375\" bgcolor=\"#000000\" type=\"application/x-shockwave-flash\"> </object>");
+	
+	//document.writeln("<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\"> <param name=\"quality\" value=\"high\"> <param name=\"wmode\" value=\"transparent\"> <param name=\"bgcolor\" value=\"#000000\"> <embed src=\"flash/maxpayne_main.swf\" quality=\"high\" width=\"750\" height=\"375\" bgcolor=\"#000000\" type=\"application/x-shockwave-flash\"> </object>");
 	
 }
